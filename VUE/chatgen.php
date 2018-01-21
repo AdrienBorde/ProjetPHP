adb60046f2f22530c8174f442c70ca8a07246e0b
Nicolas Dang <nicolas.dang@edu.esiee.fr>
2018-01-21 20:00:21 +0100

    ajout chat
    bijour

---
+++ VUE/chatgen.php
@@ -0,0 +1,37 @@
+<aside class="col-2 mx-auto" id="chatgen">
+
+
+                    <h3>chat général</h3>   
+			    <?php if(isset($_SESSION['id'])) 
+			    {
+?>
+
+			    <form action="VUE/chatgen_post.php" method="post">
+			        <p>
+			        <?php echo $_SESSION['pseudo'];
+			        ?>
+			        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
+
+			        <input type="submit" value="Envoyer" />
+				</p>
+			    </form>
+
+				<?php
+				}
+				$bdd = bdd();
+
+				// Récupération des 10 derniers messages
+				$reponse = $bdd->query('SELECT Pseudo, message FROM chatgen ORDER BY idMessage DESC LIMIT 0, 10');
+
+				// Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
+				while ($donnees = $reponse->fetch())
+				{
+					echo '<p><strong>' . htmlspecialchars($donnees['Pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+				}
+
+				$reponse->closeCursor();
+
+				?>
+          
+                </aside>
+</div>
--- index.php
+++ index.php
@@ -29,7 +29,8 @@ if (isset($_SESSION['id'])) {
 require('vue/sug_events.php');
 require('vue/map.php');
 
-require('vue/sug_amis.php');
+
+require('vue/chatgen.php');
 
 require('vue/derniermatch.php');
 require('vue/amis.php');
